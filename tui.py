343c2b3048a916b27d790cf5fea477165c7ea4c1
Bruno Rocha Moura <brunorochamoura@proton.me>
2022-11-01 15:11:12 +0000

    Improvements on TUI

--- tui.py
+++ tui.py
@@ -16,12 +16,15 @@ def draw_title(window):
 
     window.refresh()
 
-def draw_node_window(window):
+def draw_node_window(mid_x : int, mid_y : int, key : int):
     """Draws the node window, in which a user can add and remove nodes"""
 
     # The nodes in the network
     nodes_ids = list(network.nodes.keys())
-    
+
+    window = curses.newwin(20, 20, mid_y - 20//2, mid_x - 20//2)
+    window.border()
+     
     # Gets the middle x and y to be at the center of the window
     h, w = window.getmaxyx()
     def mid_x(w : int, text : str) -> int:
@@ -41,13 +44,22 @@ def draw_node_window(window):
             window.addstr(y, mid_x(w, f'Node {name}'), f'Node {name}')
             y += 1
 
+    # Get input
+    if chr(key) == ' ': # Creates node
+        network.create_node()
+
     window.refresh()
 
 def main(stdscr):
     """The main loop for drawing the TUI"""
 
+    selected_window = 'NODE'
+
     curses.curs_set(0) # Make the cursor stop blinking 
+
+    key = 0 # Starts with no input
     while True:
+
         # stdscr.clear() # Clears the terminal
         # stdscr.border() # Puts a border
 
@@ -55,22 +67,27 @@ def main(stdscr):
         mid_x = w//2
         mid_y = h//2
 
-        title_window = curses.newwin(20, 38, 5, mid_x - 38//2)
+        title_window = curses.newwin(20, 38, 1, mid_x - 38//2)
         draw_title(title_window)
 
-        node_window = curses.newwin(20, 20, mid_y - 20//2, mid_x - 20//2)
-        node_window.border()
-        draw_node_window(node_window)
-        
+        # Node window
+        if selected_window == 'NODE':
+            draw_node_window(mid_x, mid_y, key)
+
+            end_y = mid_y + 20//2
+            instructions = ['Up and down to nagivate through the list','Space for adding a node','Del for deleting highlighted node', 'Esc to go back to menu']
+
+        # Draw the instructions for the given window
+        for idx, text in enumerate(instructions):
+            stdscr.addstr(end_y + idx, mid_x - len(text)//2, text)
+            pass
+
         #stdscr.refresh()
-        stdscr.getch() # Blocks until input is given
+        key = stdscr.getch() # Blocks until input is given
 
 with open("ascii.txt", "r") as file:
     title_ascii = str(file.read())
-network = Network()
 
-network.create_node()
-network.create_node()
-network.create_node()
+network = Network()
 
 wrapper(main)
